838e8c625a88caa18d209711d6e05f9d3ba157ac
cuiqiming <qming_c@163.com>
2018-02-03 12:21:53 +0000

    初始化项目

---
+++ src/main/java/com/qiming/tooools/ToooolsApplication.java
@@ -0,0 +1,12 @@
+package com.qiming.tooools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToooolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToooolsApplication.class, args);
+	}
+}
